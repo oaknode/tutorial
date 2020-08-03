097566191e5bc842962367b195a82478db22f7dd
Marcin Gasperowicz <xnooga@gmail.com>
2020-08-03 13:19:33 +0200

    updates to the tutorial

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const dbUrl = `mongodb://${DB_HOST}:${DB_PORT}`;
 const client = new MongoClient(dbUrl);
 
 setTimeout(function () {
-  client.connect(function (err) {
-    if (err) {
-      console.log("Can't connect to the db");
-      return;
-    }
+    client.connect(function (err) {
+        if (err) {
+            console.log("Can't connect to the db " + dbUrl);
+            return;
+        }
 
-    const app = makeApp(client);
-    app.listen(PORT, HOST);
-    console.log(`Running on http://${HOST}:${PORT}`);
-  });
+        const app = makeApp(client);
+        app.listen(PORT, HOST);
+        console.log(`Running on http://${HOST}:${PORT}`);
+    });
 }, 10000);
